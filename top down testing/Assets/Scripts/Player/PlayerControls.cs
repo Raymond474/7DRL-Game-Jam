ede93c2cfb15a3eb1d373a9be2088504a79830f7
Raymond474 <61649291+Raymond474@users.noreply.github.com>
2023-03-04 16:44:02 -0800

    Added attack box and Sweeping attacks

--- top down testing/Assets/Scripts/Player/PlayerControls.cs
+++ top down testing/Assets/Scripts/Player/PlayerControls.cs
@@ -13,15 +13,22 @@ public class PlayerControls : MonoBehaviour
     public float attackRate = 2f;
     float nextAttackTime = 0f;
     public bool isRanged = false;
+    public bool isSweep = false;
+    public float attackRadius = 2f;
     public Rigidbody2D rb;
 
     public Transform attackPoint;
     public float attackLength;
     public float attackWidth;
-    public LayerMask enemeyLayers;
+    public LayerMask enemyLayers;
 
     public GameObject pauseMenuUI;
     public static bool isPaused = false;
+
+    private bool isNorth = false;
+    private bool isWest = false;
+    private bool isEast = true;
+    private Vector3 attackPosition;
 
 
     Vector2 movement;
@@ -43,27 +50,38 @@ public class PlayerControls : MonoBehaviour
         // Movement
         rb.MovePosition(rb.position + movement * movementSpeed * Time.fixedDeltaTime);
 
+        //0.71 is when going diagonal
+
         //Movement animations
 
         //right animation
         if (movement == Vector2.right)
         {
-
+            //update attack position
+            isWest = false;
+            isNorth = false;
+            isEast = true;
         }
         //left animation
         if (movement == Vector2.left)
         {
-            
+            isWest = true;
+            isNorth = false;
+            isEast = false;
         }
         //up animation
         if (movement == Vector2.up)
         {
-            
+            isWest = false;
+            isNorth = true;
+            isEast = false;
         }
         //down animation
         if (movement == Vector2.down)
         {
-            
+            isWest = false;
+            isNorth = false;
+            isEast = false;
         }
     }
 
@@ -78,17 +96,55 @@ public class PlayerControls : MonoBehaviour
             //Cooldown Check
             if (Time.time >= nextAttackTime)
             {
-                MeleeAtack();
-            }
+                float x = 0f;
+                float y = 0f;
 
-            
+                if (isWest)//facing left
+                {
+                    x = attackPoint.position.x - 1f;
+                    y = attackPoint.position.y;
+                }
+                else if (isEast)//facing right
+                {
+                    x = attackPoint.position.x + 1f;
+                    y = attackPoint.position.y;
+                }
+                else if (isNorth)
+                {
+                    x = attackPoint.position.x;
+                    y = attackPoint.position.y + 1f;
+                }
+                else
+                {
+                    x = attackPoint.position.x;
+                    y = attackPoint.position.y - 1f;
+                }
+
+                attackPosition = new Vector3(x, y, 0f);
+
+                if (isSweep)
+                {
+                    attackPosition.x = 0f;
+                    SweepAttack();
+                }
+                else
+                {
+                    MeleeAtackNotSweep();
+                }
+            }
         }
     }
 
-    private void MeleeAtack()
+    private void MeleeAtackNotSweep()
     {
         Vector2 attackRange = new Vector2(attackLength, attackWidth);
-        Collider2D[] hitEnemies = Physics2D.OverlapBoxAll(attackPoint.position, attackRange, enemeyLayers);
+
+        if (isNorth || (!isWest && !isEast))//facing North or South
+        {
+            attackRange = new Vector2(attackWidth, attackLength);
+        }
+
+        Collider2D[] hitEnemies = Physics2D.OverlapBoxAll(attackPosition, attackRange, enemyLayers);
 
         foreach(Collider2D enemy in hitEnemies)
         {
@@ -100,6 +156,72 @@ public class PlayerControls : MonoBehaviour
         }
 
         nextAttackTime = Time.time + 1f / attackRate;
+    }
+
+    private void SweepAttack()
+    {
+        Collider2D[] hitEnemies = Physics2D.OverlapCircleAll(attackPoint.position, attackRadius, enemyLayers);
+
+        foreach(Collider2D enemy in hitEnemies)
+        {
+            if (enemy.tag == "Enemy")
+            {
+                enemy.GetComponent<Enemy>().TakeDamage(attackDamage);
+            }
+                
+        }
+
+        nextAttackTime = Time.time + 1f / attackRate;
+    }
+
+    void OnDrawGizmosSelected()
+    {
+        if (attackPoint == null)
+            return;
+
+        Vector2 attackRange = new Vector2(attackLength, attackWidth);
+
+        float x = 0f;
+        float y = 0f;
+
+        //this code is not needed here. only to show real time in the editor
+        if (isWest)//facing left
+        {
+            x = attackPoint.position.x - 1f;
+            y = attackPoint.position.y;
+        }
+        else if (isEast)//facing right
+        {
+            x = attackPoint.position.x + 1f;
+            y = attackPoint.position.y;
+        }
+        else if (isNorth)
+        {
+            x = attackPoint.position.x;
+            y = attackPoint.position.y + 1f;
+
+            attackRange = new Vector2(attackWidth, attackLength);
+        }
+        else
+        {
+            x = attackPoint.position.x;
+            y = attackPoint.position.y - 1f;
+
+            attackRange = new Vector2(attackWidth, attackLength);
+        }
+
+        Vector3 attackPosition = new Vector3(x, y, 0f);
+        
+        
+
+        if (isSweep)
+        {
+            Gizmos.DrawWireSphere(attackPoint.position, attackRadius);
+        }
+        else
+        {
+            Gizmos.DrawWireCube(attackPosition, attackRange);
+        }
     }
 
     private void PauseOpen()
